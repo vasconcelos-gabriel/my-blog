cde8e343a75699bc8017c83e008ef5d0fb479331
Gabriel Vasconcelos <55930398+vasconcelos-gabriel@users.noreply.github.com>
2022-10-05 09:27:08 -0300

    inicio autenticação

---
+++ src/hooks/useAuthentication.jsx
@@ -0,0 +1,37 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut
+} from 'firebase/auth'
+
+import { useState, useEffect } from 'react'
+
+export const useAuthentication = () => {
+  const [error, setErro] = useState(null)
+  const [loading, setLoading] = useState(null)
+
+  //cleanup
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false)
+
+  const auth = getAuth()
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled()
+    setLoading(true)
+
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+}
