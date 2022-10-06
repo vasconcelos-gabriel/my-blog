4f67e414c49830834beb060abcca1fbfeaaec4de
vasconcelos1898 <55930398+vasconcelos-gabriel@users.noreply.github.com>
2022-10-06 12:08:59 -0300

    post details done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,16 @@ import styles from './Home.module.css'
 //hooks
 import { useNavigate, Link } from 'react-router-dom'
 import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 //components
+import PostDetail from '../../components/PostDetail'
 
 const Home = () => {
-  const [query, setQuery] = useState("")
-  const [posts] = useState([])
+  const [query, setQuery] = useState('')
+  const { documents: posts, loading } = useFetchDocuments('posts')
 
-
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
   }
 
@@ -20,21 +21,25 @@ const Home = () => {
     <div className={styles.home}>
       <h1>Veja os nossos posts mais recentes</h1>
       <form onSubmit={handleSubmit} className={styles.search_form}>
-        <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} 
-        value={query} />
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+        />
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>
-          Posts...
-          </h1>
-          {posts && posts.length === 0 && (
-            <div className={styles.noposts}>
-              <p>Não foram encontrados posts</p>
-              <Link to="/posts/create" className="btn">Criar primeiro post</Link>
-            </div>
-          )}
-       
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map(post => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   )
