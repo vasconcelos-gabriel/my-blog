d8a6bfe93f213882800391836cfe2f9ae8d9793a
vasconcelos1898 <55930398+vasconcelos-gabriel@users.noreply.github.com>
2022-10-06 12:56:13 -0300

    Search

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import CreatePost from './pages/CreatePost/CreatePost'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Search from './pages/Search/Search'
 
 function App() {
   const [user, setUser] = useState(undefined)
@@ -42,26 +43,26 @@ function App() {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                  path="/login"
-                  element={!user ? <Login /> : <Navigate to="/" />}
-                />
-                <Route
-                  path="/register"
-                  element={!user ? <Register /> : <Navigate to="/" />}
-                />
-                <Route
-                  path="/dashboard"
-                  element={user ? <Dashboard /> : <Navigate to="/login" />}
-                />
-                <Route
-                  path="/posts/create"
-                  element={user ? <CreatePost /> : <Navigate to="/login" />}
-                />
-              </Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              
             </Routes>
           </div>
           <Footer />
